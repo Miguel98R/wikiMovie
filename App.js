48038fa053ab2031217fe6edaf1dc936a7ee8803
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2023-10-20 11:18:03 -0600

    :cake:  create app
    create app

---
+++ App.js
@@ -0,0 +1,10 @@
+import AppNavigation from './navigation/appNavigation';
+import {Platform ,StyleSheet} from 'react-native';
+
+
+export default function App() {
+  return (
+    <AppNavigation />
+  );
+}
+
---
+++ theme/index.js
@@ -0,0 +1,9 @@
+// you can change these colors to change the look of the app ;)
+export const theme = {
+   background: '#343541',
+   text: '#932422'
+}
+export const styles = {
+   text: {color: theme.text},
+   background: {backgroundColor: theme.background}
+}
